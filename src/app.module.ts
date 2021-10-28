4477d0d99be6b8331e4c5eee53b9b074f007d750
Leo <leonardosolorzano16@gmail.com>
2021-10-28 15:01:02 -0300

    remove api module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
 import { Module } from "@nestjs/common";
-import { APIModule } from "./api/api.module";
 
 @Module({
-    imports: [APIModule],
+    imports: [],
     controllers: [],
     providers: [],
 })
